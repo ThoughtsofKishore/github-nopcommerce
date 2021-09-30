f92be15afef8f8c7efae5cb29a6686abc5f67389
kishore <kishorelalwani123@gmail.com>
2021-09-30 23:56:54 +0530

    third commit

--- testCases/abc.py
+++ testCases/abc.py
@@ -1,7 +1,4 @@
 # from selenium import webdriver
 # from selenium.webdriver.support.select import Select
 # #driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
-# driver = webdriver.Firefox(executable_path="C:\\geckodriver.exe")
-# driver.get("https://rahulshettyacademy.com/angularpractice/")
-a = 10
-
+# driver = webdriver.Firefox(executable_path="C:\\geckodriver.
